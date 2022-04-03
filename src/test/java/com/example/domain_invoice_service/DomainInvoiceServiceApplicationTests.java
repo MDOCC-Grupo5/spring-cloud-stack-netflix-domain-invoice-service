dffedee644fc225f1ff98d5700aea7bc7f42a031
Juan Sebastian Suaza Solano <juan.suaza@api-ux.com>
2022-04-02 19:33:24 -0500

    first commit

---
+++ src/test/java/com/example/domain_invoice_service/DomainInvoiceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.domain_invoice_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainInvoiceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
